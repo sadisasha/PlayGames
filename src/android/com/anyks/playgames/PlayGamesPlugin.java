e7a3587eac3ddfe0818c148ab5e053c74b25b896
sadisasha <sadisasha@gmail.com>
2014-06-26 20:19:24 +0400

    Initial commit

--- src/android/com/anyks/playgames/PlayGamesPlugin.java
+++ src/android/com/anyks/playgames/PlayGamesPlugin.java
@@ -34,46 +34,62 @@ public class PlayGamesPlugin extends CordovaPlugin implements PlayGamesServicesI
 			if ("connect".equals(action)) {
 				if (!mPlay.isConnect()) {
 	                mPlay.connect();
+	                isValidAction = true;
 	            }
 			} else if ("disconnect".equals(action)) {
 	            if (mPlay.isConnect()) {
 	                mPlay.disconnect();
+	                isValidAction = true;
 	            }
 			} else if ("reconnect".equals(action)) {
 				mPlay.reconnect();
+				isValidAction = true;
 			} else if ("statusConnection".equals(action)) {
 				String result = "false";
 				if (mPlay.isConnect()) {
 					result = "true";
+					isValidAction = true;
 				}
 				mCallbackContext.success(result);
 			} else if ("accountName".equals(action)) {
 				if (!mPlay.getAccountName().equals("")) {
 					mCallbackContext.success(mPlay.getAccountName());
+					isValidAction = true;
 				} else {
 					mCallbackContext.error("");
+					isValidAction = false;
 				}
 			} else if ("showAchievements".equals(action)) {
 				mPlay.showAchievements();
+				isValidAction = true;
 			} else if ("setIncrement".equals(action)) {
 				mPlay.setIncrement(data.getBoolean(0), data.getString(1), data.getInt(2));
+				isValidAction = true;
 			} else if ("loadAchievements".equals(action)) {
 				mPlay.loadAchievements(data.getBoolean(0));
+				isValidAction = true;
 			} else if ("setReveal".equals(action)) {
 				mPlay.setReveal(data.getBoolean(0), data.getString(1));
+				isValidAction = true;
 			} else if ("setSetSteps".equals(action)) {
 				mPlay.setSetSteps(data.getBoolean(0), data.getString(1), data.getInt(2));
+				isValidAction = true;
 			} else if ("setUnlockAchievement".equals(action)) {
 				mPlay.setUnlockAchievement(data.getBoolean(0), data.getString(1));
+				isValidAction = true;
 			} else if ("showLeaderboards".equals(action)) {
 				mPlay.showLeaderboards();
+				isValidAction = true;
 			} else if ("loadTopScores".equals(action)) {
 				mPlay.loadTopScores(data.getString(0), data.getInt(1), data.getInt(2), data.getInt(3), data.getBoolean(4));
+				isValidAction = true;
 			} else if ("setSubmitScore".equals(action)) {
 				mPlay.setSubmitScore(data.getBoolean(0), data.getString(1), data.getLong(2));
+				isValidAction = true;
 			}
 		} catch (IllegalStateException e) {
 			callbackContext.error(e.getMessage());
+			isValidAction = false;
 		} 
 		
 		return isValidAction;
