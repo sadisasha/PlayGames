3317bd745c4d841d0741c608b0e55080f5273cfd
sadisasha <sadisasha@gmail.com>
2014-07-03 15:02:07 +0400

    Initial commit

---
+++ 1.1/example/platforms/android/ant-gen/com/garpix/gpgtest/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.garpix.gpgtest;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ 1.1/example/plugins/com.anyks.playgames/www/playgames.js
@@ -0,0 +1,53 @@
+var PlayGames = function () {
+    this.options = {};
+};
+
+PlayGames.prototype.connect = function (success, failed) {
+	return cordova.exec(success, failed, "PlayGamesPlugin", "connect", [null]);
+};
+
+PlayGames.prototype.isConnected = function (success, failed) {
+	return cordova.exec(success, failed, "PlayGamesPlugin", "isConnected", [null]);
+};
+
+PlayGames.prototype.getAccountName = function (success, failed) {
+	return cordova.exec(success, failed, "PlayGamesPlugin", "getAccountName", [null]);
+};
+
+PlayGames.prototype.showAchievements = function (success, failed) {
+	return cordova.exec(success, failed, "PlayGamesPlugin", "showAchievements", [null]);
+};
+
+PlayGames.prototype.setIncrement = function (success, failed, callback, id, numSteps) {
+	return cordova.exec(success, failed, "PlayGamesPlugin", "setIncrement", [callback, id, numSteps]);
+};
+
+PlayGames.prototype.loadAchievements = function (success, failed, forceReload) {
+	return cordova.exec(success, failed, "PlayGamesPlugin", "loadAchievements", [forceReload]);
+};
+
+PlayGames.prototype.setReveal = function (success, failed, callback, id) {
+	return cordova.exec(success, failed, "PlayGamesPlugin", "setReveal", [callback, id]);
+};
+
+PlayGames.prototype.setSetSteps = function (success, failed, callback, id, numSteps) {
+	return cordova.exec(success, failed, "PlayGamesPlugin", "setSetSteps", [callback, id, numSteps]);
+};
+
+PlayGames.prototype.setUnlockAchievement = function (success, failed, callback, id) {
+	return cordova.exec(success, failed, "PlayGamesPlugin", "setUnlockAchievement", [callback, id]);
+};
+
+PlayGames.prototype.showLeaderboards = function (success, failed) {
+	return cordova.exec(success, failed, "PlayGamesPlugin", "showLeaderboards", [null]);
+};
+
+PlayGames.prototype.loadTopScores = function (success, failed, id, span, leaderboardCollection, maxResults, forceReload) {
+	return cordova.exec(success, failed, "PlayGamesPlugin", "loadTopScores", [id, span, leaderboardCollection, maxResults, forceReload]);
+};
+
+PlayGames.prototype.setSubmitScore = function (success, failed, callback, id, score) {
+	return cordova.exec(success, failed, "PlayGamesPlugin", "setSubmitScore", [callback, id, score]);
+};
+
+module.exports = new PlayGames();
